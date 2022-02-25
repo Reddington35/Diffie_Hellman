86513a8e2ad7eda99bce7d27e931453d10e56d7f
James Reddington <reddingtonjamesr@gmail.com>
2022-02-25 22:14:48 +0000

    new

--- diffieHellmen/src/diffieHellmen.java
+++ diffieHellmen/src/diffieHellmen.java
@@ -6,14 +6,12 @@ public class diffieHellmen {
         int min = 10000;
         int max = 100000;
         boolean isPrime = true;
-        for (int i = 2; i < max; i++) {
+        for (int i = min; i < max; i++) {
             Random rand = new Random();
             prime = min + rand.nextInt(max);
         }
         if(prime % 2 == 0){
             isPrime = false;
         }
-        System.out.println("practice again");
-
     }
 }
