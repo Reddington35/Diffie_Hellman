5caf604891d70c0f03d13325e77993d4a5f56d51
James Reddington <reddingtonjamesr@gmail.com>
2022-03-03 17:02:57 +0000

    Diffie Hellman Algorithm

---
+++ Diffie_Hellman/src/Diffie_Hellman.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Diffie_Hellman {
+
+    // Member Variables used
+    private int prime = 100,moduloBreak = 3,primRoot = 0;
+    private ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    // Method for generating random prime numbers
+    public int primeGenerator() {
+        for (int i = prime; i < 1000; i++) {
+            boolean foundFactor = false;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    foundFactor = true;
+                    break;
+                }
+            }
+            if (!foundFactor) {
+                arr.add(i);
+            }
+        }
+        int randomPrime = arr.get(ThreadLocalRandom.current().nextInt(0, arr.size()));
+        System.out.println("Generated Prime Number = " + randomPrime);
+        return randomPrime;
+    }
+
+    // Method for splitting large primes to prevent overflow
+    public int largeModulous(int number, int power, int modulous){
+        //System.out.println(number + " ^ " + power + " % " + modulous);
+        int count = 1;
+        number = number % modulous;
+        while(power > 0){
+            if(power % 2 == 1){
+                count = (count * number) % modulous;
+            }
+            power = power / 2;
+            number = (number * number) % modulous;
+        }
+        return count;
+    }
+
+    // Method for finding Primitive root
+    public int primitiveRoot(int prime) {
+        HashSet<Integer> prim = new HashSet<Integer>();
+        ArrayList<Integer> p = new ArrayList<Integer>();
+        for (int i = 1; i < prime; i++) {
+            ArrayList<Integer> column = new ArrayList<Integer>();
+            for (int j = 1; j < prime; j++) {
+                int count = largeModulous(i, j, prime);
+                column.add(count);
+            }
+            prim = new HashSet<Integer>(column);
+            if (column.size() == prim.size()) {
+                p.add(i);
+            }
+        }
+        System.out.println("Primitive roots are " + p);
+        System.out.println("Random Primitive root is "
+                + p.get(ThreadLocalRandom.current().nextInt(0, p.size())));
+        return p.get(ThreadLocalRandom.current().nextInt(0, p.size()));
+    }
+}
---
+++ Diffie_Hellman/src/Driver.java
@@ -0,0 +1,51 @@
+public class Driver {
+
+    // Main Method used as a Driver for DiffieHellman methods
+    public static void main(String args[]){
+
+        int XB =40,MA = 0,XA = 20;
+        int XM = 10,YM = 30;
+        Diffie_Hellman dif = new Diffie_Hellman();
+        int q = dif.primeGenerator();
+        int a = dif.primitiveRoot(q);
+
+        int YA = dif.largeModulous(a,XA,q);
+        int YB = dif.largeModulous(a,XB,q);
+
+        // Assume that YA is sent to UserB by UserA and
+        // YB is sent toi UserA by UserB
+        System.out.println("Bobs key is " + XA);
+        System.out.println("After Key Exchange, Bob's key is " + YA);
+
+        System.out.println("Alice's key is " + XB);
+        System.out.println("After Key Exchange, Alice's key is " + YB);
+
+        // Generate UserA Key
+        int KA = dif.largeModulous(YB,XA,q);
+
+        // Generate UserB key
+        int KB = dif.largeModulous(YA,XB,q);
+        System.out.println("Alice,s shared key is " + KA);
+        System.out.println("Bob,s shared key is " + KB);
+        System.out.println("Key exchange completed");
+
+        System.out.println("Simulates Man in the middle attack");
+        YM = dif.largeModulous(a,XM,q);
+
+        // Generate UserA Key
+        KA = dif.largeModulous(YM,XA,q);
+        System.out.println("Alice's Key is " + KA);
+
+        int KMa = dif.largeModulous(YA,XM,q);
+        System.out.println("Mallory,s shared key is " + KMa);
+        System.out.println("Alice Malory key exchange completed");
+
+        // Generate UserB Key
+        KB = dif.largeModulous(YM,XB,q);
+        System.out.println("Bob's Key is " + KB);
+
+        int KMb = dif.largeModulous(YB,XM,q);
+        System.out.println("Mallory shared key is " + KMb);
+        System.out.println("Malory - Bob key exchange completed");
+    }
+}
